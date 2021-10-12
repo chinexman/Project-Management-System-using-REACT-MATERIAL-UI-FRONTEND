3fdfa5a0f3c1edb9e43005eae4188277700583b2
AbuMuazu <kakupepe09@gmail.com>
2021-10-12 04:13:13 +0100

    create dev branch , did a clean up, and implemeted global styles with Css variables

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Login() {
+    return (
+       <Wrapper>
+<h1>Test</h1>
+       </Wrapper>
+    )
+}
+
+export default Login
+
+
+export const Wrapper = styled.div`
+
+`
